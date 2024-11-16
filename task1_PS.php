d8b23b124b5b5ecd1942c551af78de7a212910ef
Karas nasser <159474844+Karasnasser@users.noreply.github.com>
2024-11-16 19:53:50 +0200

    week 2_task

---
+++ task1_PS.php
@@ -0,0 +1,77 @@
+<?php
+
+/// 1
+// $s = "karas";
+// echo "hello ", $s;
+// print("<br>");
+
+
+/// 2
+// $x = 5; $y = 10;
+// echo $x, " + ",$y," = ",$x+$y,"<br>";
+// echo $x, " * ",$y," = ",$x*$y,"<br>";
+// echo $x, " - ",$y," = ",$x-$y,"<br>";
+
+
+/// 3 
+// $a=1;
+// $b=2;
+// $c=3;
+// $d=4;
+// echo "Difference = ",($a*$b)-($c*$d);
+
+
+/// 4
+// $n=13;
+// $m=12;
+// echo ($n%10)+($m%10); 
+
+
+/// 5
+// $x=4;
+// // $x=3569;
+// if($x % 2==0)
+// {
+//     print("Even");
+// }
+// else
+// {
+//     print("Odd");
+// }
+
+
+///6
+/* if both a and b are divisible by k both of them win you should print "Both". 
+If a is divisible by k but b isn't, Memo wins and you should print "Memo". 
+If b is divisible by k but a isn't, Momo wins and you should print "Momo". 
+If both a and b are not divisible by k  */
+//  $a=15;
+//  $b=7;
+//  $k=3;
+//  if($a%$k==0 && $b%$k==0){
+//     print("Both");
+//  }
+//  else if($a%$k==0 && $b%$k!=0){
+//     print("Memo");
+//  }
+//  else if($a%$k!=0 && $b%$k==0){
+//     print("Momo");
+//  }
+// else
+//     print("No one");
+
+
+/// 7
+// $num = 64;
+// $n1 = intdiv($num,10);
+// $n2 = $num % 10;
+// echo $n2," ";
+// if (($n2 != 0 && $n1 % $n2 == 0) || ($n1 != 0 && $n2 % $n1 == 0)) {
+//     print ("yes");
+// }
+// else
+// {
+//     print("no");
+// }
+
+?>
