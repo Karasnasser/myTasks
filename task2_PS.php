7cc2b94cf2f87e6507604108f8bf7561c49da419
Karas nasser <159474844+Karasnasser@users.noreply.github.com>
2024-11-16 19:54:07 +0200

    week 3_task

---
+++ task2_PS.php
@@ -0,0 +1,79 @@
+<?php
+
+/// 1
+// $num = 5;
+// for ($i=1; $i<=$num ;$i++) { 
+//     echo $i."\n";
+// }
+
+
+/// 2
+// $num = 10;
+// $num = 5;
+// for ($i=2; $i<=$num ;$i+=2) { 
+//     echo $i."\n";
+// }
+
+
+///3
+// $arr=[1,8,5,7,5];
+// $max = $arr[1];
+// for($index=0;$index<count($arr);$index++) {
+//    if ($arr[$index] > $max) {
+//     $max = $arr[$index];
+// }
+// echo "the maximum number is ",$max; 
+// }
+
+/// another solve 
+// $arr=[1,8,5,7,5];
+// $max = $arr[0];
+// foreach($arr as $value) {
+//     print($value." ");
+//     if ($max < $value)
+//     $max = $value;  
+// }
+// echo "\nthe max nums is ",$max;
+
+
+/// 4
+// $num=15;
+// $check=true;
+// for($i=2;$i<$num;$i++) {
+//     if ($num % $i ==0) {
+//         $check=false; 
+//     }
+// }
+// if ($check) {
+//         echo "Yes\n"; // prime
+//     }
+// else
+//     echo "No\n";  // Not prime
+
+
+///5
+
+
+
+///6
+// $num=4;
+// for ($i=1; $i <=$num ; $i++) { 
+//     if ($num % $i ==0) {
+//         print($i."\n");
+//     }
+// }
+
+
+///7
+
+
+
+///8
+// for ($i=0; $i <=4 ; $i++) { 
+//     for ($j=0; $j <$i ; $j++) { 
+//         print(" * ");
+//     }
+//     echo "\n";
+// }
+
+?>
